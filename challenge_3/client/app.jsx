ac012ccd4f7ac260688fd393d3db972aabdc08d2
slowmike <aznwizard123@gmail.com>
2018-07-11 15:31:29 -0700

    base client working

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,11 +1,8 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-class App extends React.Components {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      page: 1
     };
   }
 
@@ -13,21 +10,65 @@ class App extends React.Components {
 
     return (
       <div>
-        <div>
-          <F1>
-            Name:
-            <input type="text"></input>
-            Email:
-            <input type="text"></input>
-            Password:
-            <input type="text"></input>
-          </F1>
+        <div className="page">
+          <form id="F1">
+            <span>
+              Name<br/>
+              <input type="text" id="name"/><br/>
+            </span>
+            <span>
+              Email<br/>
+              <input type="text" id="email"/><br/>
+            </span>
+            <span>
+              Password<br/>
+              <input type="password" id="pass"/><br/>
+            </span>
+            <button>Next</button>
+          </form>
         </div>
-        <div>
-          <F2></F2>
+        <div className="page">
+          <form id="F2">
+            <span>
+              Street Address<br/>
+              <input type="text" id="line1"></input><br/>
+              <input type="text" id="line2"></input><br/>
+            </span>
+            <span>
+              City<br/>
+              <input type="text" id="city"></input><br/>
+            </span>
+            <span>
+              State<br/>
+              <input type="text" id="state"></input><br/>
+            </span>
+            <span>
+              Postal Code<br/>
+              <input type="text" id="postal"></input><br/>
+            </span>
+            <span>
+              Phone Number<br/>
+              <input type="text" id="phone"></input><br/>
+            </span>
+            <button>Next</button>
+          </form>
         </div>
-        <div>
-          <F3></F3>
+        <div className="page">
+          <form id="F3">
+            <span>
+              Credit Card Number<br/>
+              <input type="text" id="ccard"></input><br/>
+            </span>
+            <span>
+              CVV<br/>
+              <input type="text" id="cvv"></input><br/>
+            </span>
+            <span>
+              Billing Zip Code<br/>
+              <input type="text" id="billing"></input><br/>
+            </span>
+            <button>Next</button>
+          </form>
         </div>
       </div>
     )
