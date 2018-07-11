5856b64e17aeab88b45ff5c0e30dfcfe983345aa
slowmike <aznwizard123@gmail.com>
2018-07-11 12:30:51 -0700

    set up dependencies

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,37 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
 
+class App extends React.Components {
+  constructor(props) {
+    super(props);
+    this.state = {
 
+    };
+  }
+
+  render() {
+
+    return (
+      <div>
+        <div>
+          <F1>
+            Name:
+            <input type="text"></input>
+            Email:
+            <input type="text"></input>
+            Password:
+            <input type="text"></input>
+          </F1>
+        </div>
+        <div>
+          <F2></F2>
+        </div>
+        <div>
+          <F3></F3>
+        </div>
+      </div>
+    )
+  };
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,13 @@
+var express = require('express');
+var app = express();
 
+module.exports.app = app;
 
+app.set('port', 3000);
+
+app.use(express.static('public'));
+
+if(!module.parent) {
+  app.listen(app.get('port'));
+  console.log('Listening on', app.get('port'));
+}
